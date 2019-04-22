792cf86c7ca855b37d672c2f14a4668e50b91685
Zhengxi Liu <39415743+xcmyz@users.noreply.github.com>
2019-04-22 17:02:32 +0800

    Add files via upload

--- layers.py
+++ layers.py
@@ -12,28 +12,16 @@ def add_cls_sep(text):
 
 def get_bert_embedding(text, model, tokenizer, return_token=False):
     text = add_cls_sep(text)
-    # print(text)
     tokenized_text = tokenizer.tokenize(text)
-    # print(tokenized_text)
     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-    # print(indexed_tokens)
     segments_ids = [0 for i in range(len(indexed_tokens))]
-    # print(segments_ids)
     tokens_tensor = torch.tensor([indexed_tokens])
     segments_tensors = torch.tensor([segments_ids])
 
     with torch.no_grad():
         encoded_layers, _ = model(tokens_tensor, segments_tensors)
 
-    # print(torch.Tensor(encoded_layers))
-    # print(len(encoded_layers))
-    # print(len(encoded_layers[0]))
-    # print(len(encoded_layers[0][0]))
-    # print(len(encoded_layers[0][0][0]))
     output = encoded_layers[11][0]
-    # print(len(output))
-    # print(len(output[0]))
-    # print(output[0])
 
     if return_token:
         return output, tokenized_text
@@ -94,6 +82,36 @@ class LinearNet(nn.Module):
         return out
 
 
+class LinearNet_TwoLayer(nn.Module):
+    """
+    Linear Network for two layers
+    """
+
+    def __init__(self, input_size, hidden_size, output_size, dropout=0.1):
+        super(LinearNet_TwoLayer, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.layer_1 = nn.Linear(self.input_size, self.hidden_size)
+        self.dropout_1 = nn.Dropout(dropout)
+        self.layer_2 = nn.Linear(self.hidden_size, self.output_size)
+        self.dropout_2 = nn.Dropout(dropout)
+
+        nn.init.xavier_uniform_(self.layer_1.weight,
+                                gain=nn.init.calculate_gain("linear"))
+        nn.init.xavier_uniform_(self.layer_2.weight,
+                                gain=nn.init.calculate_gain("linear"))
+
+    def forward(self, x):
+        x = self.layer_1(x)
+        x = self.dropout_1(x)
+        x = self.layer_2(x)
+        output = self.dropout_2(x)
+
+        return output
+
+
 class FFN(nn.Module):
     """
     Feed Forward Network
@@ -127,22 +145,63 @@ class LinearProjection(nn.Module):
     Predict Gate
     """
 
-    def __init__(self, input_size, output_size, hidden_size=256, dropout=0.5, bias=True, w_init_gain="sigmoid"):
+    def __init__(self, input_size, hidden_size, output_size, dropout=0.1, w_init_gain="sigmoid"):
         # :param input_size: dimension of input
         # :param output_size: dimension of output
 
         super(LinearProjection, self).__init__()
-        self.linear_layer = nn.Linear(input_size, output_size, bias=bias)
         self.input_size = input_size
-        self.output_size = output_size
         self.hidden_size = hidden_size
+        self.output_size = output_size
 
-        nn.init.xavier_uniform_(self.linear_layer.weight,
-                                gain=nn.init.calculate_gain(w_init_gain))
+        self.linear_layer_1 = nn.Linear(self.input_size, self.hidden_size)
+        self.sigmoid_1 = nn.Sigmoid()
+        self.dropout = nn.Dropout(dropout)
+        self.linear_layer_2 = nn.Linear(self.hidden_size, self.output_size)
+        self.sigmoid_2 = nn.Sigmoid()
+
+        nn.init.xavier_uniform_(
+            self.linear_layer_1.weight, gain=nn.init.calculate_gain(w_init_gain))
+        nn.init.xavier_uniform_(
+            self.linear_layer_2.weight, gain=nn.init.calculate_gain(w_init_gain))
 
     def forward(self, x):
-        output = self.linear_layer(x)
-        # output = torch.softmax(output, dim=2)
+        x = self.linear_layer_1(x)
+        x = self.sigmoid_1(x)
+        x = self.dropout(x)
+        x = self.linear_layer_2(x)
+        output = self.sigmoid_2(x)
 
         # print(output)
         return output
+
+
+if __name__ == "__main__":
+    # Test
+    linear_projection = LinearProjection(80, 256, 1)
+    test_input = torch.randn(2, 123, 80)
+    test_output = linear_projection(test_input)
+    # print(test_output.size())
+    # print(test_output)
+
+    # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    # model_bert = BertModel.from_pretrained('bert-base-uncased')
+
+    # text = "I am writing a paper for NeurISP."
+    # test_embeddings = get_bert_embedding(text, model_bert, tokenizer)
+    # print(test_embeddings)
+
+    test_input = torch.randn(2, 123, 60)
+    # print(test_input)
+
+    test_FFN = FFN(60, 256, 3)
+    test_output = test_FFN(test_input)
+    # print(test_output)
+
+    test_LN_T = LinearNet_TwoLayer(80, 256, 70)
+    test_input = torch.randn(2, 167, 80)
+    test_output = test_LN_T(test_input)
+    print(test_output.size())
+
+    print(test_input)
+    print(test_output)
